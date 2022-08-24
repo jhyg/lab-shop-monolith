7c5b1718ad3e52ff6cfa5c898f9b128a643c26aa
jhyg <65217813+jhyg@users.noreply.github.com>
2022-08-24 17:50:49 +0900

    commit msg

--- monolith/src/main/java/labshopmonolith/domain/Order.java
+++ monolith/src/main/java/labshopmonolith/domain/Order.java
@@ -79,6 +79,8 @@ public class Order  {
 
 
 
+    public void test(){
+    }
 
 
 
--- monolith/src/main/java/labshopmonolith/infra/OrderHateoasProcessor.java
+++ monolith/src/main/java/labshopmonolith/infra/OrderHateoasProcessor.java
@@ -1,17 +1,20 @@
 package labshopmonolith.infra;
-
 import labshopmonolith.domain.*;
-import org.springframework.hateoas.EntityModel;
+
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.RepresentationModelProcessor;
 import org.springframework.stereotype.Component;
+import org.springframework.hateoas.EntityModel;
 
 @Component
-public class OrderHateoasProcessor
-    implements RepresentationModelProcessor<EntityModel<Order>> {
+public class OrderHateoasProcessor implements RepresentationModelProcessor<EntityModel<Order>>  {
 
     @Override
     public EntityModel<Order> process(EntityModel<Order> model) {
+        model.add(Link.of(model.getRequiredLink("self").getHref() + "/test").withRel("test"));
+
+        
         return model;
     }
+    
 }
